4c42f41af6a6d996ba63bcaf10e6da287f8dc869
Danil <nikulizadan@mail.ru>
2025-12-22 17:41:40 +0300

    create Api

---
+++ Api/src/errors/exeption.filter.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { LoggerService } from "../logger/logger.service";
+import { IExeprionFilter } from "./exeption.filter.interface";
+import { HTTPError } from "./http-error";
+
+export class ExeprionFilter implements IExeprionFilter {
+  logger: LoggerService;
+  constructor(logger: LoggerService) {
+    this.logger = logger;
+  }
+  catch(
+    err: Error | HTTPError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (err instanceof HTTPError) {
+      this.logger.error(
+        `[${err.context}] Ошибка ${err.statusCode} : ${err.message}`
+      );
+      res.status(err.statusCode).send({ err: err.message });
+    } else {
+      this.logger.error(`${err.message}`);
+      res.status(500).send({ err: err.message });
+    }
+  }
+}
---
+++ Api/src/users/user.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error";
+
+export class UserController extends BaseController {
+    constructor(logger: LoggerService) {
+    super(logger);
+    this.bindRoutes([
+      { path: '/login', method: 'post', func: this.login },
+      { path: '/register', method: 'post', func: this.register }
+    ]);
+  }
+
+  login(req: Request, res: Response, next: NextFunction)  {
+    next(new HTTPError(401,'Ошибка авторизации', 'login'))
+  }
+
+   register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, 'register')
+  }
+}
